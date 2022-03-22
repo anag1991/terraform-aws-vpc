ada3ce8f78d4ae71101e2d643b6ceabb7c735278
anaghirghilijiu <96264142+anaghirghilijiu@users.noreply.github.com>
2022-03-21 19:26:59 -0500

    added subnets

--- vpc copy.tf
+++ vpc copy.tf
@@ -1,9 +1,3 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-  tags       = var.tags
-}
-
-
 resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnets_cidr[0]
